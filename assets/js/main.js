be07f3daac4b8a0909a05a7de6122da7e929dd77
Aryan Agrawal <ary4n99@gmail.com>
2021-04-28 21:30:26 +0100

    Remove artificial delay on initial render

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,9 +24,7 @@
 
   // Play initial animations on page load.
   $window.on("load", function () {
-    window.setTimeout(function () {
-      $body.removeClass("is-preload");
-    }, 100);
+    $body.removeClass("is-preload");
   });
 
   // Nav.

7fb4392b886d1133481621eb29fa9a6c47dfecfd
Aryan Agrawal <ary4n99@gmail.com>
2021-08-20 17:16:01 +0200

    Improve favicons

---
+++ js/redirect.js
@@ -0,0 +1,8 @@
+if (
+  !(
+    window.location.hostname.includes("aryanagrawal.com") ||
+    window.location.hostname.includes("127.0.0.1")
+  )
+) {
+  location.href = "https://aryanagrawal.com/";
+}

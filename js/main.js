85ad126733e863e51fe0d5173e11efad5d818ca6
Aryan Agrawal <ary4n99@gmail.com>
2021-09-09 18:58:00 +0200

    Improve performance drastically

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 /* ===================================================================
  * Ceevee 2.0.0 - Main JS
- *
  * ------------------------------------------------------------------- */
 
 (function (html) {
@@ -24,11 +23,6 @@
         }
       });
     });
-
-    // force page scroll position to top at page refresh
-    // window.addEventListener('beforeunload' , function () {
-    //     window.scrollTo(0, 0);
-    // });
   }; // end ssPreloader
 
   /* Parallax
@@ -36,42 +30,6 @@
   const ssParallax = function () {
     const rellax = new Rellax(".rellax");
   }; // end ssParallax
-
-  /* Move header menu
-   * -------------------------------------------------- */
-  const ssMoveHeader = function () {
-    const hdr = document.querySelector(".s-header");
-    const hero = document.querySelector("#hero");
-    let triggerHeight;
-
-    if (!(hdr && hero)) return;
-
-    setTimeout(function () {
-      triggerHeight = hero.offsetHeight - 170;
-    }, 300);
-
-    window.addEventListener("scroll", function () {
-      let loc = window.scrollY;
-
-      if (loc > triggerHeight) {
-        hdr.classList.add("sticky");
-      } else {
-        hdr.classList.remove("sticky");
-      }
-
-      if (loc > triggerHeight + 20) {
-        hdr.classList.add("offset");
-      } else {
-        hdr.classList.remove("offset");
-      }
-
-      if (loc > triggerHeight + 150) {
-        hdr.classList.add("scrolling");
-      } else {
-        hdr.classList.remove("scrolling");
-      }
-    });
-  }; // end ssMoveHeader
 
   /* Mobile Menu
    * ---------------------------------------------------- */
@@ -109,68 +67,6 @@
     });
   }; // end ssMobileMenu
 
-  /* Highlight active menu link on pagescroll
-   * ------------------------------------------------------ */
-  const ssScrollSpy = function () {
-    const sections = document.querySelectorAll(".target-section");
-
-    // Add an event listener listening for scroll
-    window.addEventListener("scroll", navHighlight);
-
-    function navHighlight() {
-      // Get current scroll position
-      let scrollY = window.pageYOffset;
-
-      // Loop through sections to get height(including padding and border),
-      // top and ID values for each
-      sections.forEach(function (current) {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute("id");
-
-        /* If our current scroll position enters the space where current section
-         * on screen is, add .current class to parent element(li) of the thecorresponding
-         * navigation link, else remove it. To know which link is active, we use
-         * sectionId variable we are getting while looping through sections as
-         * an selector
-         */
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-          document
-            .querySelector(".s-header__nav a[href*=" + sectionId + "]")
-            .parentNode.classList.add("current");
-        } else {
-          document
-            .querySelector(".s-header__nav a[href*=" + sectionId + "]")
-            .parentNode.classList.remove("current");
-        }
-      });
-    }
-  }; // end ssScrollSpy
-
-  /* Swiper
-   * ------------------------------------------------------ */
-  const ssSwiper = function () {
-    const mySwiper = new Swiper(".swiper-container", {
-      slidesPerView: 1,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      breakpoints: {
-        // when window width is >= 401px
-        401: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        // when window width is >= 801px
-        801: {
-          slidesPerView: 2,
-          spaceBetween: 48,
-        },
-      },
-    });
-  }; // end ssSwiper
-
   /* Lightbox
    * ------------------------------------------------------ */
   const ssLightbox = function () {
@@ -201,25 +97,6 @@
     });
   }; // end ssLightbox
 
-  /* Alert boxes
-   * ------------------------------------------------------ */
-  const ssAlertBoxes = function () {
-    const boxes = document.querySelectorAll(".alert-box");
-
-    boxes.forEach(function (box) {
-      box.addEventListener("click", function (e) {
-        if (e.target.matches(".alert-box__close")) {
-          e.stopPropagation();
-          e.target.parentElement.classList.add("hideit");
-
-          setTimeout(function () {
-            box.style.display = "none";
-          }, 500);
-        }
-      });
-    });
-  }; // end ssAlertBoxes
-
   /* Smoothscroll
    * ------------------------------------------------------ */
   const ssSmoothScroll = function () {
@@ -236,39 +113,13 @@
     });
   }; // end ssSmoothScroll
 
-  /* back to top
-   * ------------------------------------------------------ */
-  const ssBackToTop = function () {
-    const pxShow = 900;
-    const goTopButton = document.querySelector(".ss-go-top");
-
-    if (!goTopButton) return;
-
-    // Show or hide the button
-    if (window.scrollY >= pxShow) goTopButton.classList.add("link-is-visible");
-
-    window.addEventListener("scroll", function () {
-      if (window.scrollY >= pxShow) {
-        if (!goTopButton.classList.contains("link-is-visible"))
-          goTopButton.classList.add("link-is-visible");
-      } else {
-        goTopButton.classList.remove("link-is-visible");
-      }
-    });
-  }; // end ssBackToTop
-
   /* initialize
    * ------------------------------------------------------ */
   (function ssInit() {
     ssPreloader();
     ssParallax();
-    ssMoveHeader();
     ssMobileMenu();
-    ssScrollSpy();
-    ssSwiper();
     ssLightbox();
-    ssAlertBoxes();
     ssSmoothScroll();
-    ssBackToTop();
   })();
 })(document.documentElement);
